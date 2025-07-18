9c7840d043cb09f285fdf77f25fd3aeffc0e9cbf
imnadsa <antosh1109@mail.ru>
2025-07-18 18:21:41 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,24 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'styrene-a': ['var(--font-styrene-a)', 'sans-serif'],
+        'styrene-b': ['var(--font-styrene-b)', 'sans-serif'],
+        'tiempo': ['var(--font-tiempo)', 'serif'],
+      },
+      colors: {
+        // Цвета добавим позже
+      },
+      animation: {
+        // Анимации добавим позже
+      }
+    },
+  },
+  plugins: [],
+}
