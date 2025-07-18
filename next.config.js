ef981809907f8fe5420bfafd45b8197a4f3173f8
imnadsa <antosh1109@mail.ru>
2025-07-18 18:21:05 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ['lucide-react']
+  },
+  images: {
+    formats: ['image/webp', 'image/avif'],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+}
 
+module.exports = nextConfig
