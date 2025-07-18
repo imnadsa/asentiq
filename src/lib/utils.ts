f03002c265c86ad03b7bcf110301569dc39d499e
imnadsa <antosh1109@mail.ru>
2025-07-18 18:27:32 +0400

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,1 +1,21 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatNumber(num: number): string {
+  if (num >= 1000) {
+    return `${(num / 1000).toFixed(1)}k`
+  }
+  return num.toString()
+}
+
+export function formatDate(date: string | Date): string {
+  return new Intl.DateTimeFormat('ru-RU', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  }).format(new Date(date))
+}
