519c31c71113fb52ab533726a6cfc5868cf6cbe6
imnadsa <antosh1109@mail.ru>
2025-07-19 16:28:18 +0400

    Update fonts.ts

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -3,17 +3,17 @@ import localFont from 'next/font/local'
 export const styreneA = localFont({
   src: [
     {
-      path: '../../public/fonts/Styrene A Regular Trial.woff2',
+      path: '../../../public/fonts/Styrene A Regular Trial.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/Styrene A Medium Trial.woff2',
+      path: '../../../public/fonts/Styrene A Medium Trial.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/Styrene A Bold Trial.woff2',
+      path: '../../../public/fonts/Styrene A Bold Trial.woff2',
       weight: '700',
       style: 'normal',
     },
@@ -25,17 +25,17 @@ export const styreneA = localFont({
 export const styreneB = localFont({
   src: [
     {
-      path: '../../public/fonts/Styrene B Regular Trial.woff2',
+      path: '../../../public/fonts/Styrene B Regular Trial.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/Styrene B Medium Trial.woff2',
+      path: '../../../public/fonts/Styrene B Medium Trial.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/Styrene B Bold Trial.woff2',
+      path: '../../../public/fonts/Styrene B Bold Trial.woff2',
       weight: '700',
       style: 'normal',
     },
@@ -47,17 +47,17 @@ export const styreneB = localFont({
 export const tiempoText = localFont({
   src: [
     {
-      path: '../../public/fonts/Test Tiempos Text Regular.woff2',
+      path: '../../../public/fonts/Test Tiempos Text Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/Test Tiempos Text Medium.woff2',
+      path: '../../../public/fonts/Test Tiempos Text Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/Test Tiempos Text Bold.woff2',
+      path: '../../../public/fonts/Test Tiempos Text Bold.woff2',
       weight: '700',
       style: 'normal',
     },
